b591e716525f4cb7b67bafb8b7d078a083017b70
lizy <eagle.lzy@gmail.com>
2016-03-02 17:01:20 +0800

    05 examples

---
+++ app/05/str_cli1.c
@@ -0,0 +1,15 @@
+#include "../../include/unp.h"
+
+void
+str_cli(FILE *fp, int sockfd) {
+	char sendline[MAXLINE], recvline[MAXLINE];
+	while (Fgets(sendline, MAXLINE, fp) != NULL) {
+		Writen(sockfd, sendline, strlen(MAXLINE));
+
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
+
+		Fputs(recvline, stdout);
+	}
+}
+
